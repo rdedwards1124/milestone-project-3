f25dcd87e311381668f079c907f0566789656f7f
Rashonda Edwards <rashondaedwards@aol.com>
2023-09-12 23:20:54 -0700

    more for searchpage

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -1,4 +1,7 @@
 import "../Add-Style/SearchPage.css";
+import electric from '../Other/electric.png'
+import water from '../Other/water.png'
+import fire from '../Other/fire.png'
 
 function SearchPage() {
     return (
@@ -34,7 +37,86 @@ function SearchPage() {
                 </div>
                 <div>
                     <h2>Search By Type</h2>
-                    <div></div>
+                    <div className="types">
+                <div>
+                    {/* <!-- <h2>Electric</h2> --> */}
+                    <a href="">
+                        <img src={electric} alt="electric symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Water</h2> --> */}
+                    <a href="">
+                        <img src={water} alt="water symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Fire</h2> --> */}
+                    <a href="">
+                        <img src={fire} alt="fire symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Grass</h2> --> */}
+                    <a href="">
+                        <img src="../assets/Other/grass.png" alt="grass symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Ice</h2> --> */}
+                    <a href="">
+                        <img src="../assets/Other/ice.png" alt="ice symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Rock</h2> --> */}
+                    <a href="">
+                        <img src="../assets/Other/rock.png" alt="rock symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Ground</h2> --> */}
+                    <a href=""><img src="../assets/Other/ground.png" alt="ground symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Steel</h2> --> */}
+                    <a href=""><img src="../assets/Other/steel.png" alt="steel symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Poison</h2> --> */}
+                    <a href=""><img src="../assets/Other/poison.png" alt="poison symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Fairy</h2> --> */}
+                    <a href=""><img src="../assets/Other/fairy.png" alt="fairy symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Dark</h2> --> */}
+                    <a href=""><img src="../assets/Other/dark.png" alt="dark symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Psychic</h2> --> */}
+                    <a href=""><img src="../assets/Other/psychic.png" alt="psychic symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Fighting</h2> --> */}
+                    <a href=""><img src="../assets/Other/fighting.png" alt="fighting symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Flying</h2> --> */}
+                    <a href=""><img src="../assets/Other/flying.png" alt="flying symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Bug</h2> --> */}
+                    <a href=""><img src="../assets/Other/bug.png" alt="bug symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Ghost</h2> --> */}
+                    <a href=""><img src="../assets/Other/ghost.png" alt="ghost symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Normal</h2> --> */}
+                    <a href=""><img src="../assets/Other/normal.png" alt="normal symbol"/></a>
+                </div>
+                <div>
+                    {/* <!-- <h2>Dragon</h2> --> */}
+                    <a href=""><img src="../assets/Other/dragon.png" alt="dragon symbol"/></a>
+                </div>
+            </div> 
                 </div>
             </div>
         </>
