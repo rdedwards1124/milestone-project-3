1b9eca0ae1b5f5c22f0e4a89ab74bf5525fa6b68
Rashonda Edwards <rashondaedwards@aol.com>
2023-09-12 19:04:23 -0700

    no change...

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-// app.get("/", (req,res) => {
-//     res.send("Hello World")
-// })
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.PORT}`)
