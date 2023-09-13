f24aeb2b20f6e2326c59b2771dd22aa9084b89bd
Rashonda Edwards <rashondaedwards@aol.com>
2023-09-13 16:28:57 -0700

    more for the user-my-page

--- frontend/src/pages/UserMyPage.js
+++ frontend/src/pages/UserMyPage.js
@@ -3,9 +3,9 @@ import "../Add-Style/UserMyPage.css";
 function UserMyPage() {
     return (
         <>
-            <div>
+            <div className="CenterIt">
                 <h1>USERNAME</h1>
-                <div className="SearchedPoke">
+                <div className="UserDetails">
                     <div className="SearchedPokeImg">
                         <img
                             width="90%"
@@ -22,6 +22,34 @@ function UserMyPage() {
                         </div>
                     </div>
                 </div>
+                <div className="BattleTeams">
+                    <h2>Battle Team</h2>
+                    <div>
+                        <h3>Pokemon</h3>
+                        <img src="https://i.etsystatic.com/33357979/r/il/e1dfcd/3584257734/il_fullxfull.3584257734_bfy9.jpg"></img>
+                    </div>
+                    <div>
+                        <h3>Pokemon</h3>
+                        <img src="https://i.etsystatic.com/33357979/r/il/e1dfcd/3584257734/il_fullxfull.3584257734_bfy9.jpg"></img>
+                    </div>
+                    <div>
+                        <h3>Pokemon</h3>
+                        <img src="https://i.etsystatic.com/33357979/r/il/e1dfcd/3584257734/il_fullxfull.3584257734_bfy9.jpg"></img>
+                    </div>
+                    <div>
+                        <h3>Pokemon</h3>
+                        <img src="https://i.etsystatic.com/33357979/r/il/e1dfcd/3584257734/il_fullxfull.3584257734_bfy9.jpg"></img>
+                    </div>
+                    <div>
+                        <h3>Pokemon</h3>
+                        <img src="https://i.etsystatic.com/33357979/r/il/e1dfcd/3584257734/il_fullxfull.3584257734_bfy9.jpg"></img>
+                    </div>
+                    <div>
+                        <h3>Pokemon</h3>
+                        <img src="https://i.etsystatic.com/33357979/r/il/e1dfcd/3584257734/il_fullxfull.3584257734_bfy9.jpg"></img>
+                    </div>
+                </div>
+                <div></div>
             </div>
         </>
     );
