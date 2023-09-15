3af064ac4fc23342462ce34800c9b44d28c6a1b6
Rashonda Edwards <rashondaedwards@aol.com>
2023-09-15 00:12:36 -0700

    no breaks so far!

--- frontend/src/pages/SearchType.js
+++ frontend/src/pages/SearchType.js
@@ -6,6 +6,8 @@ import AllTypes from "./AllTypes";
 function SearchType (){
     const { id } = useParams();
     const [typeData, setTypeData] = useState({})
+    const [searchValue, setSearchValue] = useState("");
+    const [searchedPokemon, setSearchedPokemon] = useState({});
 
     const getTypeInfo = async (ID) => {
         const url = `https://pokeapi.co/api/v2/type/${ID}/`;
@@ -16,8 +18,18 @@ function SearchType (){
         }
     };
 
+    const getPokemon = async (searchValue) => {
+        const url = `https://pokeapi.co/api/v2/pokemon/${searchValue}`;
+        const response = await fetch(url);
+        const responseJSON = await response.json();
+        if (responseJSON) {
+            setSearchedPokemon(responseJSON);
+        }
+    };
+
     useEffect(() => {
         getTypeInfo(id);
+        getPokemon(searchValue)
     }, [id]);
 
     return(
